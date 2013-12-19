ba211f1b21007abb9344dbbd6724dc372f967e66
Marco E <marco.esposito@gmail.com>
2013-12-19 23:56:04 +0100

    add Trade struct

--- kmarketdata.cpp
+++ kmarketdata.cpp
@@ -11,6 +11,26 @@ using namespace std;
 using namespace Kraken;
 
 //------------------------------------------------------------------------------
+// deals with Trades:
+struct Trade {
+   double price, volume; 
+   time_t time; 
+   char order;
+};
+
+//------------------------------------------------------------------------------
+// helper function to load a Trade from a JSONNode:
+Trade get_trade(const JSONNode& node) 
+{
+   Trade t; 
+   t.price  = node[0].as_float();
+   t.volume = node[1].as_float();
+   t.time   = node[2].as_int();
+   t.order  = node[3].as_string()[0];
+   return t;
+}
+
+//------------------------------------------------------------------------------
 
 int main() 
 { 
@@ -21,7 +41,7 @@ int main()
       KAPI::Input in;
 
       // get recent trades 
-      in.insert(make_pair("pair", "XXBTZEUR"));
+      in.insert(make_pair("pair", "XLTCZEUR"));
 
       string json_trades = kapi.public_method("Trades", in); 
       JSONNode root = libjson::parse(json_trades);
@@ -41,26 +61,23 @@ int main()
       }
       else {
 	 // format the output in columns: time, order, price and volume
-	 JSONNode result_node = root.at("result")[0];
+	 JSONNode result = root.at("result")[0];
 	 
-	 for (JSONNode::const_iterator it = result_node.begin();
-	      it != result_node.end(); ++it) {
-	    const double price  = (*it)[0].as_float();
-	    const double volume = (*it)[1].as_float();
-	    const time_t time   = (*it)[2].as_int();
-	    const char order    = (*it)[3].as_string()[0];
+	 for (JSONNode::const_iterator it = result.begin();
+	      it != result.end(); ++it) {
+	    Trade t = get_trade(*it); 
 
 	    struct tm timeinfo;
-	    localtime_r(&time, &timeinfo);
+	    localtime_r(&t.time, &timeinfo);
 
 	    char buffer[20];
 	    strftime(buffer, 20, "%T", &timeinfo);
 
 	    cout << left << setw(12) << buffer << "   "
-		 << left << setw(4) << (order=='b'?"buy":"sell") << "   " 
+		 << left << setw(1) << t.order << "   " 
 		 << fixed << right 
-		 << setprecision(5) << price << "   " 
-		 << setprecision(9) << volume << endl;
+		 << setprecision(5) << t.price << "   " 
+		 << setprecision(9) << t.volume << endl;
 	 }
       }
 
