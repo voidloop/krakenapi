635474e7baa05a6955a290774be64b05d1e21ea2
Marco E <marco.esposito@gmail.com>
2013-12-29 16:31:13 +0100

    now the program "krt" downloads recent trades from Kraken.com and print outs data in CSV format

--- krt.cpp
+++ krt.cpp
@@ -4,6 +4,9 @@
 #include <stdexcept>
 #include <ctime>
 
+#include <chrono>
+#include <thread>
+
 #include "kapi.hpp"
 #include "libjson/libjson.h"
 
@@ -106,6 +109,81 @@ ostream& operator<<(ostream& os, const KAsset_pair& a)
 	     << '"';
 }
 
+
+//------------------------------------------------------------------------------
+// deals with Kraken trades:
+struct KTrade {
+   double price, volume; 
+   time_t time; 
+   char order;
+
+   KTrade(JSONNode node) {
+      price  = node[0].as_float();
+      volume = node[1].as_float();
+      time   = node[2].as_int();
+      order  = node[3].as_string()[0];
+   }
+};
+
+//------------------------------------------------------------------------------
+// prints out a kraken trade:
+ostream& operator<<(ostream& os, const KTrade& t) 
+{
+   return os << '"'
+	     << t.time << "\",\""
+	     << t.order << "\",\""
+	     << fixed
+	     << setprecision(5) << t.price << "\",\""
+	     << setprecision(9) << t.volume << '"';
+}
+
+//------------------------------------------------------------------------------
+// throws an runtime_error if there are errors in the JSON response 
+void check_kraken_errors(const JSONNode& root)
+{
+   if (!root.at("error").empty()) {
+      std::ostringstream oss;
+      oss << "Kraken response contains errors: ";
+      
+      // append errors to output string stream
+      for (JSONNode::const_iterator it = root["error"].begin(); 
+	   it != root["error"].end(); ++it) 
+	 oss << endl << " * " << libjson::to_std_string(it->as_string());
+      
+      throw runtime_error(oss.str());
+   }
+} 
+
+//------------------------------------------------------------------------------
+// downloads recent trades:
+string recent_trades(const KAPI& k, const KAPI::Input& i, vector<KTrade>& v)
+{
+   string json_data = k.public_method("Trades", i);
+   JSONNode root = libjson::parse(json_data);
+   //cout << json_data << endl;
+
+   // throw an exception if there are errors in the JSON response
+   check_kraken_errors(root);
+
+   // throw an exception if result is empty   
+   if (root.at("result").empty()) {
+      throw runtime_error("Kraken response doesn't contain result data");
+   }
+
+   const string& pair = i.at("pair");
+
+   JSONNode& result = root["result"];
+   JSONNode& result_pair = result.at(pair);
+
+   vector<KTrade> output;
+   for (JSONNode::iterator it = result_pair.begin(); 
+	it != result_pair.end(); ++it)
+      output.push_back(KTrade(*it));
+      
+   output.swap(v);
+   return libjson::to_std_string( result.at("last").as_string() );
+}
+
 //------------------------------------------------------------------------------
 
 int main() 
@@ -114,32 +192,25 @@ int main()
 
    try {
       KAPI kapi;
+      KAPI::Input input;
+      
+      std::chrono::seconds dura(30);
+      std::vector<KTrade> trades;
 
-      string json_data = kapi.public_method("AssetPairs", KAPI::Input()); 
-      JSONNode root = libjson::parse(json_data);
+      input["pair"] = "XXBTZEUR";
+      input["since"] = "0";
 
-      // check for errors, if they're present an 
-      // exception will be thrown
-      if (!root.at("error").empty()) {
-	 ostringstream oss;
-	 oss << "Kraken response contains errors: ";
-
-	 // append errors to output string stream
-	 for (JSONNode::iterator it = root["error"].begin(); 
-	      it != root["error"].end(); ++it) 
-	    oss << endl << " * " << libjson::to_std_string(it->as_string());
+      while (true) {
+	 // store and print trades
+	 const string last = recent_trades(kapi, input, trades);
+	 for (int i = 0; i < trades.size(); ++i) 
+	    cout << trades[i] << endl;
+	    
+	 // next "since" is the "last" value
+	 input["since"] = last;
 	 
-	 throw runtime_error(oss.str());
-      }
-      else {
-
-	 JSONNode result = root.at("result");
-	 
-	 for (JSONNode::iterator it = result.begin(); 
-	      it != result.end(); ++it)
-	 {
-	    cout << KAsset_pair(*it) << endl;
-	 }
+	 // sleep
+	 std::this_thread::sleep_for(dura);
       }
    }
    catch(exception& e) {
