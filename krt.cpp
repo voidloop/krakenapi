ad6a6a069571412b1b3ea8ce57015122d0dd5eef
Marco E <marco.esposito@gmail.com>
2014-01-05 12:57:22 +0100

    removed main.cpp and added KClient to krt.cpp

--- krt.cpp
+++ krt.cpp
@@ -7,7 +7,7 @@
 #include <chrono>
 #include <thread>
 
-#include "kapi.hpp"
+#include "kraken/kclient.hpp"
 #include "libjson/libjson.h"
 
 using namespace std;
@@ -109,81 +109,6 @@ ostream& operator<<(ostream& os, const KAsset_pair& a)
 	     << '"';
 }
 
-
-//------------------------------------------------------------------------------
-// deals with Kraken trades:
-struct KTrade {
-   double price, volume; 
-   time_t time; 
-   char order;
-
-   KTrade(JSONNode node) {
-      price  = node[0].as_float();
-      volume = node[1].as_float();
-      time   = node[2].as_int();
-      order  = node[3].as_string()[0];
-   }
-};
-
-//------------------------------------------------------------------------------
-// prints out a kraken trade:
-ostream& operator<<(ostream& os, const KTrade& t) 
-{
-   return os << '"'
-	     << t.time << "\",\""
-	     << t.order << "\",\""
-	     << fixed
-	     << setprecision(5) << t.price << "\",\""
-	     << setprecision(9) << t.volume << '"';
-}
-
-//------------------------------------------------------------------------------
-// throws an runtime_error if there are errors in the JSON response 
-void check_kraken_errors(const JSONNode& root)
-{
-   if (!root.at("error").empty()) {
-      std::ostringstream oss;
-      oss << "Kraken response contains errors: ";
-      
-      // append errors to output string stream
-      for (JSONNode::const_iterator it = root["error"].begin(); 
-	   it != root["error"].end(); ++it) 
-	 oss << endl << " * " << libjson::to_std_string(it->as_string());
-      
-      throw runtime_error(oss.str());
-   }
-} 
-
-//------------------------------------------------------------------------------
-// downloads recent trades:
-string recent_trades(const KAPI& k, const KAPI::Input& i, vector<KTrade>& v)
-{
-   string json_data = k.public_method("Trades", i);
-   JSONNode root = libjson::parse(json_data);
-   //cout << json_data << endl;
-
-   // throw an exception if there are errors in the JSON response
-   check_kraken_errors(root);
-
-   // throw an exception if result is empty   
-   if (root.at("result").empty()) {
-      throw runtime_error("Kraken response doesn't contain result data");
-   }
-
-   const string& pair = i.at("pair");
-
-   JSONNode& result = root["result"];
-   JSONNode& result_pair = result.at(pair);
-
-   vector<KTrade> output;
-   for (JSONNode::iterator it = result_pair.begin(); 
-	it != result_pair.end(); ++it)
-      output.push_back(KTrade(*it));
-      
-   output.swap(v);
-   return libjson::to_std_string( result.at("last").as_string() );
-}
-
 //------------------------------------------------------------------------------
 
 int main(int argc, char* argv[]) 
@@ -200,51 +125,50 @@ int main(int argc, char* argv[])
       //     krt <pair> [interval] [since]
       // 
 
-      KAPI::Input input;
-      input["since"] = "0"; // by default: the oldest possible trade data
+      string pair;
+      string last = "0"; // by default: the oldest possible trade data
       int interval = 0;   // by default: krt exits after download trade data
 
       switch (argc) {
       case 4:
-	 input["since"] = string(argv[3]);
+	 last = string(argv[3]);
       case 3:
 	 istringstream(argv[2]) >> interval;      
       case 2:
-	 input["pair"] = string(argv[1]);
+	 pair = string(argv[1]);
 	 break;
       default: 
-	 throw std::runtime_error("wrong number of arguments");
+	 throw runtime_error("wrong number of arguments");
       };
-
-      KAPI kapi;
       
-      std::chrono::seconds dura(interval);
-      std::vector<KTrade> trades;
+      chrono::seconds dura(interval);
+
+      KClient kc;
+      vector<KTrade> vt;
 
       while (true) {
 	 // store and print trades
-	 const string last = recent_trades(kapi, input, trades);
-	 for (int i = 0; i < trades.size(); ++i) 
-	    cout << trades[i] << endl;
+	 last = kc.trades(pair, last, vt);
+	 for (int i = 0; i < vt.size(); ++i) 
+	    cout << vt[i] << endl;
 	    
-	 // next "since" is the "last" value
-	 input["since"] = last;
-
 	 // exit from the loop if interval is 0
 	 if (interval == 0) break;
-
+	 
 	 // sleep
-	 std::this_thread::sleep_for(dura);
-      } 
+	 this_thread::sleep_for(dura);
+      }
 
       // terminate kraken lib's resources
       Kraken::terminate();
    }
    catch(exception& e) {
       cerr << "Error: " << e.what() << endl;
+      exit(EXIT_FAILURE);
    }
    catch(...) {
       cerr << "Unknow exception." << endl;
+      exit(EXIT_FAILURE);
    }
 
    return 0;
